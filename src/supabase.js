96ef8f1a044c78e3a22767b2aadf485cf16e58ee
Yingying Sun <yingying.sun214@gmail.com>
2024-02-13 16:38:00 +1100

    initial commit

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://oqnhweotpoteqiihrwpf.supabase.co";
-const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9xbmh3ZW90cG90ZXFpaWhyd3BmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDcwODg5MTcsImV4cCI6MjAyMjY2NDkxN30.J1x35YD_dLfTQHh0Yw50cBjKs1A_2NlyUliaB_boHiI";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
